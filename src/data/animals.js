e93439704e983fe16923c733b1a6b9921c278715
Yeseo0502 <5252yeseo@gmail.com>
2026-06-14 21:17:58 +0900

    json수정

--- src/data/animals.js
+++ src/data/animals.js
@@ -1,34 +1,3 @@
-import img31 from "../assets/image31.png";
-import img32 from "../assets/image32.jpg";
-import img33 from "../assets/image33.jpg";
-import img34 from "../assets/image34.png";
-import img35 from "../assets/image35.jpg";
-import img36 from "../assets/image36.jpg";
-import img37 from "../assets/image37.jpg";
-import img38 from "../assets/image38.jpg";
-import img39 from "../assets/image39.png";
-import img40 from "../assets/image40.png";
-import img41 from "../assets/image41.png";
-import img42 from "../assets/image42.jpg";
-import img43 from "../assets/image43.png";
-import img44 from "../assets/image44.png";
-import img45 from "../assets/image45.png";
-import img46 from "../assets/image46.png";
-import img47 from "../assets/image47.png";
-import img48 from "../assets/image48.png";
-import img49 from "../assets/image49.jpg";
-import img50 from "../assets/image50.jpg";
-import img51 from "../assets/image51.jpg";
-import img52 from "../assets/image52.jpg";
-import img53 from "../assets/image53.png";
-import img54 from "../assets/image54.jpg";
-import img55 from "../assets/image55.jpg";
-import img56 from "../assets/image56.jpg";
-import img57 from "../assets/image57.png";
-import img58 from "../assets/image58.jpg";
-import img59 from "../assets/image59.jpg";
-import img60 from "../assets/image60.jpg";
-
 const animals = [
   {
     id: 1,
@@ -301,7 +270,7 @@ const animals = [
   {
     id: 31,
     name: "악어",
-    image: img31,
+    image: "",
     description1:
       "평균 사이즈 : 450cm | 평균 몸무게 : 500kg | 주 서식지 : 열대 강과 습지",
     description2: "강력한 턱과 긴 꼬리를 가진 대형 파충류",
@@ -310,7 +279,7 @@ const animals = [
   {
     id: 32,
     name: "카멜레온",
-    image: img32,
+    image: "",
     description1:
       "평균 사이즈 : 30cm | 평균 몸무게 : 0.2kg | 주 서식지 : 마다가스카르",
     description2: "주변 환경에 맞춰 몸 색깔을 바꾸는 파충류",
@@ -319,7 +288,7 @@ const animals = [
   {
     id: 33,
     name: "하이에나",
-    image: img33,
+    image: "",
     description1:
       "평균 사이즈 : 140cm | 평균 몸무게 : 60kg | 주 서식지 : 아프리카 초원",
     description2: "강한 턱 힘과 사회성을 가진 포식자",
@@ -328,7 +297,7 @@ const animals = [
   {
     id: 34,
     name: "닥터피쉬",
-    image: img34,
+    image: "",
     description1:
       "평균 사이즈 : 10cm | 평균 몸무게 : 10g | 주 서식지 : 터키, 이란의 온천 및 하천",
     description2: "사람의 피부 각질을 먹는 독특한 습성을 가진 작은 물고기",
@@ -337,7 +306,7 @@ const animals = [
   {
     id: 35,
     name: "바다사자",
-    image: img35,
+    image: "",
     description1:
       "평균 사이즈 : 220cm | 평균 몸무게 : 300kg | 주 서식지 : 태평양 연안",
     description2: "수영 실력이 뛰어나고 지능이 높은 해양 포유류",
@@ -346,7 +315,7 @@ const animals = [
   {
     id: 36,
     name: "물개",
-    image: img36,
+    image: "",
     description1:
       "평균 사이즈 : 170cm | 평균 몸무게 : 120kg | 주 서식지 : 한대 해역",
     description2: "유선형 몸을 가진 해양 포유류",
@@ -355,7 +324,7 @@ const animals = [
   {
     id: 37,
     name: "카피바라",
-    image: img37,
+    image: "",
     description1:
       "평균 사이즈 : 130cm | 평균 몸무게 : 55kg | 주 서식지 : 남아메리카 습지",
     description2: "세계에서 가장 큰 설치류",
@@ -364,7 +333,7 @@ const animals = [
   {
     id: 38,
     name: "아르마딜로",
-    image: img38,
+    image: "",
     description1: "평균 사이즈 : 75cm | 평균 몸무게 : 6kg | 주 서식지 : 중남미",
     description2: "단단한 갑옷 같은 피부를 가진 포유류",
     zoo: ["서울대공원","아르티스 테마파크"],
@@ -372,7 +341,7 @@ const animals = [
   {
     id: 39,
     name: "나무늘보",
-    image: img39,
+    image: "",
     description1:
       "평균 사이즈 : 65cm | 평균 몸무게 : 5kg | 주 서식지 : 남아메리카 열대우림",
     description2: "매우 느린 움직임으로 유명한 포유류",
@@ -381,7 +350,7 @@ const animals = [
   {
     id: 40,
     name: "라쿤",
-    image: img40,
+    image: "",
     description1:
       "평균 사이즈 : 50~70cm | 평균 몸무게 : 5~10kg | 주 서식지 : 북미 및 중미의 숲과 침엽수림",
     description2: "귀여운 외모와 달리 호기심이 많고, 먹이를 물에 씻어 먹는 버릇이 있는 잡식 동물",
@@ -390,7 +359,7 @@ const animals = [
   {
     id: 41,
     name: "들소",
-    image: img41,
+    image: "",
     description1:
       "평균 사이즈 : 300cm | 평균 몸무게 : 900kg | 주 서식지 : 북아메리카 초원",
     description2: "거대한 어깨 혹이 특징인 초식동물",
@@ -399,7 +368,7 @@ const animals = [
   {
     id: 42,
     name: "야크",
-    image: img42,
+    image: "",
     description1:
       "평균 사이즈 : 280cm | 평균 몸무게 : 600kg | 주 서식지 : 티베트 고원",
     description2: "추위에 강한 긴 털의 소과 동물",
@@ -408,7 +377,7 @@ const animals = [
   {
     id: 43,
     name: "순록",
-    image: img43,
+    image: "",
     description1:
       "평균 사이즈 : 180cm | 평균 몸무게 : 150kg | 주 서식지 : 북극권",
     description2: "암수 모두 뿔이 나는 사슴",
@@ -417,7 +386,7 @@ const animals = [
   {
     id: 44,
     name: "뱀",
-    image: img44,
+    image: "",
     description1:
       "평균 사이즈 : 100~300cm | 평균 몸무게 : 1~15kg | 주 서식지 : 전 세계의 숲, 사막, 초지 (실내 사육 환경)",
     description2: "독성이 있는 파충류",
@@ -426,7 +395,7 @@ const animals = [
   {
     id: 45,
     name: "앵무새",
-    image: img45,
+    image: "",
     description1:
       "평균 사이즈 : 10~100cm | 평균 몸무게 : 60g~1.5kg | 주 서식지 : 열대 및 아열대 우림",
     description2: "사람의 말을 흉내 내며 지능이 매우 높은 화려한 깃털의 조류",
@@ -435,7 +404,7 @@ const animals = [
   {
     id: 46,
     name: "사슴",
-    image: img46,
+    image: "",
     description1:
       "평균 사이즈 : 180cm | 평균 몸무게 : 120kg | 주 서식지 : 숲과 초원",
     description2: "민첩한 움직임과 뿔이 특징인 초식동물",
@@ -444,7 +413,7 @@ const animals = [
   {
     id: 47,
     name: "멧돼지",
-    image: img47,
+    image: "",
     description1:
       "평균 사이즈 : 150cm | 평균 몸무게 : 100kg | 주 서식지 : 산림",
     description2: "강한 주둥이로 땅을 파헤치는 잡식동물",
@@ -453,7 +422,7 @@ const animals = [
   {
     id: 48,
     name: "라마",
-    image: img48,
+    image: "",
     description1:
       "평균 사이즈 : 180cm | 평균 몸무게 : 140kg | 주 서식지 : 안데스 산맥",
     description2: "짐을 운반하는 데 이용되는 낙타과 동물",
@@ -462,7 +431,7 @@ const animals = [
   {
     id: 49, 
     name: "돌산양",
-    image: img49,
+    image: "",
     description1:
       "평균 사이즈 : 130~180cm | 평균 몸무게 : 45~110kg | 주 서식지 : 알래스카 및 캐나다 북서부의 험준한 고산 지대",
     description2: "험난한 바위 절벽을 민첩하게 달릴 수 있으며, 수컷의 크고 둥글게 말린 뿔이 특징인 동물",
@@ -471,7 +440,7 @@ const animals = [
   {
     id: 50,
     name: "고슴도치",
-    image: img50,
+    image: "",
     description1:
       "평균 사이즈 : 25cm | 평균 몸무게 : 1kg | 주 서식지 : 유럽·아시아",
     description2: "몸을 둥글게 말아 자신을 보호하는 포유류",
@@ -480,7 +449,7 @@ const animals = [
   {
     id: 51,
     name: "토끼",
-    image: img51,
+    image: "",
     description1: "평균 사이즈 : 40cm | 평균 몸무게 : 2kg | 주 서식지 : 초원",
     description2: "긴 귀와 빠른 번식력으로 유명한 초식동물",
     zoo: ["서울대공원","어린이대공원"],
@@ -488,7 +457,7 @@ const animals = [
   {
     id: 52,
     name: "기니피그",
-    image: img52,
+    image: "",
     description1:
       "평균 사이즈 : 25cm | 평균 몸무게 : 1kg | 주 서식지 : 남아메리카",
     description2: "온순한 성격의 설치류",
@@ -497,7 +466,7 @@ const animals = [
   {
     id: 53,
     name: "친칠라",
-    image: img53,
+    image: "",
     description1:
       "평균 사이즈 : 30cm | 평균 몸무게 : 0.7kg | 주 서식지 : 안데스 산맥",
     description2: "매우 부드러운 털을 가진 설치류",
@@ -506,7 +475,7 @@ const animals = [
   {
     id: 54,
     name: "다람쥐",
-    image: img54,
+    image: "",
     description1:
       "평균 사이즈 : 60cm | 평균 몸무게 : 6kg | 주 서식지 : 산악지대",
     description2: "겨울잠을 자는 대형 설치류",
@@ -515,7 +484,7 @@ const animals = [
   {
     id: 55,
     name: "퓨마",
-    image: img55,
+    image: "",
     description1:
       "평균 사이즈 : 180cm | 평균 몸무게 : 70kg | 주 서식지 : 북·남아메리카",
     description2: "뛰어난 점프 능력을 가진 대형 고양이과 동물",
@@ -524,7 +493,7 @@ const animals = [
   {
     id: 56,
     name: "스라소니",
-    image: img56,
+    image: "",
     description1:
       "평균 사이즈 : 100cm | 평균 몸무게 : 18kg | 주 서식지 : 북반구 산림",
     description2: "귀 끝의 긴 털이 특징인 야생 고양이",
@@ -533,7 +502,7 @@ const animals = [
   {
     id: 57,
     name: "말레이곰",
-    image: img57,
+    image: "",
     description1:
       "평균 사이즈 : 130cm | 평균 몸무게 : 50kg | 주 서식지 : 동남아시아 열대우림",
     description2: "곰 중 가장 작은 종으로 긴 혀를 가지고 있다",
@@ -542,7 +511,7 @@ const animals = [
   {
     id: 58,
     name: "프레리도그",
-    image: img58,
+    image: "",
     description1:
       "평균 사이즈 : 35cm | 평균 몸무게 : 1kg | 주 서식지 : 북아메리카 초원",
     description2: "땅굴을 파며 집단생활을 하는 설치류",
@@ -551,7 +520,7 @@ const animals = [
   {
     id: 59,
     name: "사막여우",
-    image: img59,
+    image: "",
     description1:
       "사이즈 : 30cm | 몸무게 : 3kg | 서식지 : 사하라 사막",
     description2: "귀가 매우 크고 발바닥에 털이 있어 모래 위를 잘 걷는 동물",
@@ -560,7 +529,7 @@ const animals = [
   {
     id: 60,
     name: "왈라루",
-    image: img60,
+    image: "",
     description1: "평균 사이즈 : 140cm | 평균 몸무게 : 45kg | 주 서식지 : 호주",
     description2: "캥거루와 왈라비의 중간 크기를 가진 유대류",
     zoo: ["서울대공원","청주동물원"],
